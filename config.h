1f063b5c4bf1706ca9093f90472b2acb33261639
paradox <31886084+paradox-R@users.noreply.github.com>
2020-01-14 16:39:27 +0530

    Center Screen Prompt

--- config.h
+++ config.h
@@ -2,17 +2,16 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Cascadia Code:pixelsize=13:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-//#include "/home/paradox/.cache/wal/colors-wal-dmenu.h"
 static const char *colors[SchemeLast][2] = {
-		     //     fg         bg       
+	/*     fg         bg       */
 	[SchemeNorm] = { "#FEFEFE", "#181617" },
-	[SchemeSel] = { "#E4312C", "#181617" },
+	[SchemeSel] = { "#181617", "#E4312C" },
 	[SchemeOut] = { "#FEFEFE", "#181617" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -25,4 +24,4 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 1;
+static const unsigned int border_width = 2;
